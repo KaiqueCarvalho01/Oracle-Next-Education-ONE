906de08960be55f2f0d4b59efd00169773e062f0
Kaique Carvalho <kaiquecarvalho479jogos@gmail.com>
2025-07-23 18:19:00 -0300

    Organizando estruturas das pastas

---
+++ Logica de Programacao/logica-js-projeto_inicial/Modulo 1/Exercicios/app.js
@@ -0,0 +1,16 @@
+//Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+alert("Boas vindas ao nosso site!");
+
+//Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = "Lua";
+//Crie uma variável chamada idade e atribua a ela o valor 25.
+let idade = 25;
+//Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let numeroDeVendas = 50;
+//Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+let saldoDisponivel = 1000;
+//Exiba um alerta com o texto "Erro! Preencha todos os campos"
+alert("Erro! Preencha todos os campos");
+//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
