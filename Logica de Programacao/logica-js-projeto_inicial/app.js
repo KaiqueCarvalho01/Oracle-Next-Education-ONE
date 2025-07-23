325360e673f7cad897ea62112dd4b6b227358d96
Kaique Carvalho <kaiquecarvalho479jogos@gmail.com>
2025-07-23 18:07:55 -0300

    Instalação projeto e inicialização do curso

---
+++ Logica de Programacao/logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 29;
+let chute = prompt("Escolha um numero entre 1 e 30");
+
+if (chute == numeroSecreto) {
+  console.log("Parabéns, você acertou!");
+}
